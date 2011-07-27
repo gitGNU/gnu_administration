f889afa23fbe4cbe723ed62deec2869b00536301
Michael J. Flickinger <mjflick@gnu.org>
2011-07-27 01:48:57 -0400

    allow for non-mgt keys to be replicated on all savannah vm's

--- authorized_keys_replicate.sh
+++ authorized_keys_replicate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 # Copy mgt vm's keys to the other instances
-scp -pr ~root/.ssh/authorized_keys root@frontend.savannah.gnu.org:.ssh/
-scp -pr ~root/.ssh/authorized_keys root@internal.savannah.gnu.org:.ssh/
-scp -pr ~root/.ssh/authorized_keys root@download.savannah.gnu.org:/etc/ssh/authorized_keys/root
-scp -pr ~root/.ssh/authorized_keys root@vcs.savannah.gnu.org:/etc/ssh/authorized_keys/root
+scp -pr ~root/.ssh/vm_authorized_keys root@frontend.savannah.gnu.org:.ssh/authorized_keys
+scp -pr ~root/.ssh/vm_authorized_keys root@internal.savannah.gnu.org:.ssh/authorized_keys
+scp -pr ~root/.ssh/vm_authorized_keys root@download.savannah.gnu.org:/etc/ssh/authorized_keys/root
+scp -pr ~root/.ssh/vm_authorized_keys root@vcs.savannah.gnu.org:/etc/ssh/authorized_keys/root
