4f28d6572df784bd74f61e259d09019c80a6ebc9
Michael J. Flickinger <mjflick@gnu.org>
2011-07-27 02:03:51 -0400

    update scripts for mgt vm

--- maintenance/add_user_to_project.pl
+++ maintenance/add_user_to_project.pl
@@ -8,23 +8,23 @@ my $is_admin = (grep { $_ eq 'admin'  } @ARGV) ? 'A' : '';
 die "usage: $0 [group] [user_to_add] [admin_name] onduty admin\n"
     if not $group or not $user or not $admin_name;
 
-my (undef, $group_id)=`ssh internal "mysql savane -B -e 'SELECT group_id FROM groups WHERE unix_group_name=\\"$group\\";'"`;
+my (undef, $group_id)=`ssh root@internal "mysql savane -B -e 'SELECT group_id FROM groups WHERE unix_group_name=\\"$group\\";'"`;
 die "project $group does not exist\n" unless $group_id;
 
-my (undef, $user_id)=`ssh internal "mysql savane -B -e 'SELECT user_id FROM user WHERE user_name=\\"$user\\";'"`;
+my (undef, $user_id)=`ssh root@internal "mysql savane -B -e 'SELECT user_id FROM user WHERE user_name=\\"$user\\";'"`;
 die "user $user does not exist\n" unless $user_id;
 
-my (undef, $admin_id)=`ssh internal "mysql savane -B -e 'SELECT user_id FROM user WHERE user_name=\\"$admin_name\\";'"`;
+my (undef, $admin_id)=`ssh root@internal "mysql savane -B -e 'SELECT user_id FROM user WHERE user_name=\\"$admin_name\\";'"`;
 die "admin $user does not exist\n" unless $admin_id;
 
-my (undef, $already_added)=`ssh internal "mysql savane -B -e 'SELECT user_group_id FROM user_group WHERE user_id=$user_id AND group_id=$group_id;'"`;
+my (undef, $already_added)=`ssh root@internal "mysql savane -B -e 'SELECT user_group_id FROM user_group WHERE user_id=$user_id AND group_id=$group_id;'"`;
 
 die "$user is already added to $group (use the web interface for permission changes)\n"
     if $already_added;
 
-`ssh internal "mysql savane -B -e 'INSERT INTO user_group (user_id, group_id, onduty, admin_flags, cache_user_name) VALUES ($user_id, $group_id, $onduty, \\"$is_admin\\", \\"$user\\");'"`;
+`ssh root@internal "mysql savane -B -e 'INSERT INTO user_group (user_id, group_id, onduty, admin_flags, cache_user_name) VALUES ($user_id, $group_id, $onduty, \\"$is_admin\\", \\"$user\\");'"`;
 
 my $time = time;
-`ssh internal "mysql savane -B -e 'INSERT INTO group_history (group_id, field_name, old_value, mod_by, date) VALUES ($group_id, \\"Added User\\", \\"$user\\", $admin_id, $time);'"`;
+`ssh root@internal "mysql savane -B -e 'INSERT INTO group_history (group_id, field_name, old_value, mod_by, date) VALUES ($group_id, \\"Added User\\", \\"$user\\", $admin_id, $time);'"`;
 
 print "$user added to $group by $admin_name\n";
--- maintenance/delete_project.sh
+++ maintenance/delete_project.sh
@@ -5,44 +5,44 @@
 group=$1
 if [ -z "$group" -o "$group" = "--help" -o "$group" = "-h" ]; then echo "Usage: $0 group_name"; exit 1; fi
 #if [ ! -e "/vservers/cvs/web/$group" ]; then echo "Group $group does not exist."; exit 1; fi
-id=$(ssh root@internal.in.sv.gnu.org "mysql savane -B -e \"SELECT group_id FROM groups where unix_group_name='$group';\"" | tail -n +2)
+id=$(ssh root@internal "mysql savane -B -e \"SELECT group_id FROM groups where unix_group_name='$group';\"" | tail -n +2)
 
-ssh root@internal.in.sv.gnu.org "perl -MSavane -e 'DeleteGroup($group);'"
+ssh root@internal "perl -MSavane -e 'DeleteGroup($group);'"
 if [ ! -z "$id" ]; then
-    ssh root@internal.in.sv.gnu.org "mysql savane -e \"DELETE FROM trackers_watcher WHERE group_id='$id';\""
+    ssh root@internal "mysql savane -e \"DELETE FROM trackers_watcher WHERE group_id='$id';\""
 fi
 
 backup_dir=~/deleted-projects/$group
 mkdir $backup_dir
 
-ssh root@vcs-noshell.in.sv.gnu.org chattr -i /srv/cvs/sources/$group/CVSROOT
-rsync -aHS root@vcs-noshell.in.sv.gnu.org:/srv/cvs/sources/$group/ $backup_dir/sources/
-ssh root@vcs-noshell.in.sv.gnu.org rm -rf /srv/cvs/sources/$group/
+ssh root@vcs chattr -i /srv/cvs/sources/$group/CVSROOT
+rsync -aHS root@vcs:/srv/cvs/sources/$group/ $backup_dir/sources/
+ssh root@vcs rm -rf /srv/cvs/sources/$group/
 
 rsync -aHS root@vcs-noshell.in.sv.gnu.org:/srv/cvs/web/$group/ $backup_dir/web/
 # Empty the website - no need to ask sysadmin that way
-ssh root@vcs-noshell.in.sv.gnu.org find /srv/cvs/web/$group/$group -type f -print0 \| xargs -0 --no-run-if-empty rm
-echo "No more there." | ssh root@vcs-noshell.in.sv.gnu.org cat \> /srv/cvs/web/$group/$group/index.html
-ssh root@vcs-noshell.in.sv.gnu.org ci -q -m'-' /srv/cvs/web/$group/$group/index.html < /dev/null
+ssh root@vcs find /srv/cvs/web/$group/$group -type f -print0 \| xargs -0 --no-run-if-empty rm
+echo "No more there." | ssh root@vcs cat \> /srv/cvs/web/$group/$group/index.html
+ssh root@vcs ci -q -m'-' /srv/cvs/web/$group/$group/index.html < /dev/null
 curl http://www.gnu.org/new-savannah-project/new.py -F type=non-gnu -F project=$group
-ssh root@vcs-noshell.in.sv.gnu.org chattr -i /srv/cvs/web/$group/CVSROOT
-ssh root@vcs-noshell.in.sv.gnu.org rm -rf /srv/cvs/web/$group/
-ssh root@vcs-noshell.in.sv.gnu.org rm -rf /var/lock/cvs/web/$group
-ssh root@vcs-noshell.in.sv.gnu.org rm -rf /var/lock/cvs/sources/$group
+ssh root@vcs chattr -i /srv/cvs/web/$group/CVSROOT
+ssh root@vcs rm -rf /srv/cvs/web/$group/
+ssh root@vcs rm -rf /var/lock/cvs/web/$group
+ssh root@vcs rm -rf /var/lock/cvs/sources/$group
 
-rsync -aHS root@sftp.in.sv.gnu.org:/srv/download/$group/ $backup_dir/download/
-ssh root@sftp.in.sv.gnu.org rm -rf /srv/download/$group/
+rsync -aHS root@download:/srv/download/$group/ $backup_dir/download/
+ssh root@download rm -rf /srv/download/$group/
 
-ssh root@vcs-noshell.in.sv.gnu.org chattr -i /srv/git/$group.git/hooks
-rsync -aHS root@vcs-noshell.in.sv.gnu.org:/srv/git/$group.git/ $backup_dir/$group.git/
-ssh root@vcs-noshell.in.sv.gnu.org rm -rf /srv/git/$group.git/
+ssh root@vcs chattr -i /srv/git/$group.git/hooks
+rsync -aHS root@vcs:/srv/git/$group.git/ $backup_dir/$group.git/
+ssh root@vcs rm -rf /srv/git/$group.git/
 
-ssh root@vcs-noshell.in.sv.gnu.org chattr -f -i /srv/git/$group/*/hooks
-rsync -aHS root@vcs-noshell.in.sv.gnu.org:/srv/git/$group/ $backup_dir/git/
-ssh root@vcs-noshell.in.sv.gnu.org rm -rf /srv/git/$group/
+ssh root@vcs chattr -f -i /srv/git/$group/*/hooks
+rsync -aHS root@vcs:/srv/git/$group/ $backup_dir/git/
+ssh root@vcs rm -rf /srv/git/$group/
 
-rsync -aHS root@vcs-noshell.in.sv.gnu.org:/srv/hg/$group/ $backup_dir/hg/
-ssh root@vcs-noshell.in.sv.gnu.org rm -rf /srv/hg/$group/
+rsync -aHS root@vcs:/srv/hg/$group/ $backup_dir/hg/
+ssh root@vcs rm -rf /srv/hg/$group/
 
 
 # GPG Keyring
